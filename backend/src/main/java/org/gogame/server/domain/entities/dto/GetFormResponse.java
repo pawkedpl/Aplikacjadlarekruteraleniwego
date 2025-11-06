d88eac92c9a4520ddf4e553ef0f8c55c4046005c
Romek Szlachtun <lidroma2@gmail.com>
2025-11-06 20:33:27 +0100

    merge backends

--- backend/src/main/java/org/gogame/server/domain/entities/dto/GetFormResponse.java
+++
@@ -1,9 +0,0 @@
-package org.gogame.server.domain.entities.dto;
-
-import lombok.Builder;
-import org.gogame.server.domain.entities.QuestionEntity;
-
-import java.util.List;
-
-@Builder
-public record GetFormResponse(List<QuestionEntity> questions) {}
--- backend/src/main/java/org/gogame/server/domain/entities/dto/SubmitFormResponse.java
+++
@@ -1,8 +0,0 @@
-package org.gogame.server.domain.entities.dto;
-
-import lombok.Builder;
-
-import java.util.UUID;
-
-@Builder
-public record SubmitFormResponse(UUID uuid) {}
