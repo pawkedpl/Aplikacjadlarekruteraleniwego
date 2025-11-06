be10d59e666c011d4191e1ae642406ba6d5bd451
Paweł Kędzierski <147321628+pawkedpl@users.noreply.github.com>
2025-11-06 19:52:19 +0100

    Delete BackendFrondendwersja2 directory

--- BackendFrondendwersja2/src/test/java/org/gogame/server/controllers/FormControllerTests.java
+++
@@ -1,8 +0,0 @@
-package org.gogame.server.controllers;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-@WebMvcTest(FormController.class)
-class FormControllerTests {
-    // todo later
-}
